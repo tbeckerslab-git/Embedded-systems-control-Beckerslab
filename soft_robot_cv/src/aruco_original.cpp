3fb81bfd8711309cfac27746dc2cd4dcf39ea671
GloriaXM <gloria.i.moudou@vanderbilt.edu>
2024-01-02 00:15:19 -0500

    calc cam pose

--- soft_robot_cv/src/aruco_original.cpp
+++ soft_robot_cv/src/aruco_original.cpp
@@ -11,12 +11,6 @@ using namespace std;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-  //Initialize camera and distortion parameters
-  float data[9] = { 1.05326155e+03, 0.00000000e+00, 6.72839741e+02, 0.00000000e+00, 1.05471479e+03, 3.55441462e+02, 0, 0, 1.00000000e+00};
-  cv::Mat cameraMatrix = cv::Mat(3, 3, CV_32F, data);
-
-  float data2[5] = { 0.09004139, -0.29306343, -0.00608111, 0.00926377, 0.2020092};
-  cv::Mat distCoeffs = cv::Mat(1, 5, CV_32F, data2);
  
   // Pointer used for the conversion from a ROS message to 
   // an OpenCV-compatible image
@@ -40,37 +34,15 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
 
-  //Detect the marker
   std::vector<int> markerIds;
   std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+  // cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters();
   const cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+  // cv::aruco::ArucoDetector detector(dictionary, detectorParams);
   cv::aruco::detectMarkers(current_frame, dictionary, markerCorners, markerIds); //  cv::aruco::detectMarkers(current_frame, dictionary, markerCorners, markerIds); //
-  cv::aruco::drawDetectedMarkers(current_frame, markerCorners, markerIds); 
 
-  //Calculate pose of marker where rvecs is rotation and tvecs is translation with respect to the camera lens
-  std::vector<cv::Vec3d> rvecs, tvecs;
-  cv::Mat objPoints(4, 1, CV_32FC3);
-  cv::aruco::estimatePoseSingleMarkers(markerCorners, 0.078, cameraMatrix, distCoeffs, rvecs, tvecs, objPoints); //Marker side length is 0.078 meters
-  
-  for (auto elem : tvecs) {
-    cout << elem << ", ";
-  }
-
-  cout << endl;
-  
-  // cv::drawFrameAxes(current_frame, cameraMatrix, distCoeffs, rvecs, tvecs, 0.1);
-  // cout << objPoints << endl;
-  // std::vector<cv::Vec3d> rvecs, tvecs;
-
-  // // Set coordinate system
-  // cv::Mat objPoints(4, 1, CV_32FC3);
-  // cv::aruco::drawDetectedMarkers(current_frame, markerCorners, markerIds); 
-  // solvePnP(objPoints, markerCorners.at(0), cameraMatrix, distCoeffs, rvecs.at(0), tvecs.at(0));
-  // // for (int i = 0; i < rvecs.size(); ++i) {
-  // //   auto rvec = rvecs[i];
-  // //   auto tvec = tvecs[i];
-  // //   cv::drawFrameAxes(current_frame, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
-  // // }
+  // cv::aruco::estimatePoseSingleMarkers(markerCorners, 0.5, current_frame);
+  cv::aruco::drawDetectedMarkers(current_frame, markerCorners, markerIds);
 
   // Display the current frame
   cv::imshow("view",current_frame); 
