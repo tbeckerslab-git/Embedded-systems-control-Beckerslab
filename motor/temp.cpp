1119a3528542f3a4d6008bac77e10c13c2f26bdc
User <tbeckerslab@gmail.com>
2025-02-27 16:58:10 -0600

    refactor and delete unused code

--- motor/temp.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    // Print "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
-
-    return 0; // Return 0 to indicate successful execution
-}
